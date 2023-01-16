ac2e47f13227a0af5daf4eb05f39cbe60c708bf3
Kal Bartal <118667094+KalBartal@users.noreply.github.com>
2023-01-16 15:32:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Round the result to 2 decimal places.
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+# Tip Calculator
 
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+This is my coding challenge submission for day #2 of the 100 Days of Code: The Complete Python Pro Bootcamp, instructor-led training.
 
-print(f"Each person should pay: ${final_amount}")
+This is a Python program to calculate how much each person should pay when splitting a bill with tip included.
+
+## Requirements
+
+- Python 3 or higher
+
+## Installation
+
+No installation is necessary to run this program.
+
+## Usage
+
+Clone or download this repository. Run the `main.py` file in your Python terminal. 
+
+## Example
+
+```bash
+$ python main.py
+Welcome to the tip calculator!
+What was the total bill? $150.00
+How much tip would you like to give? 10, 12, or 15? 12
+How many people to split the bill? 5
+Each person should pay: $33.6
+```
+
